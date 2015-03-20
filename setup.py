10944d8ca71edeab12ba7e8f5469f61eef51f390
alfred@tid.es <alfred@tid.es>
2015-03-20 13:55:29 +0100

    Better format

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='dirty-models-sphinx',
     url='https://github.com/alfred82santa/dirty-models-sphinx',
     author='alfred82santa',
-    version='0.1.1',
+    version='0.1.2',
     author_email='alfred82santa@gmail.com',
     packages=['dirty_models_sphinx'],
     install_requires=['dirty-models', 'sphinx'],
